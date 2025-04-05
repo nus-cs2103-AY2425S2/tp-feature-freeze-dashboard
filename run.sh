abc4e45f4b9777b9b5ff99f25c43e7d7b08e4b54
Damith C. Rajapakse <1673303+damithc@users.noreply.github.com>
2025-04-05 19:16:00 +0800

    Fix date format

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
 ./get-reposense.py --commit a0e771ee53d0cae52d244c893b134010aa4fde9e
-java -jar RepoSense.jar --config ./config --since 01-04-2025  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
+java -jar RepoSense.jar --config ./config --since 05/04/2025  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
